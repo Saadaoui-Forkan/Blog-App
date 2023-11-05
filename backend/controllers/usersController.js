7289894a1289df3c26f4fa2139f86ca28bf5cece
unknown <forkan.saadaoui@gmail.com>
2023-11-05 22:07:31 +0100

    get user profile

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -4,15 +4,31 @@ const { User } = require('../models/User')
 /**-----------------------------------------------
  * @desc    Get All Users Profile
  * @route   /api/users/profile
- * @method  GET
+ * @method  GET 
  * @access  private (only admin)
  ------------------------------------------------*/
  const getUsersController = asyncHandler (async(req, res) => {
-   console.log(req.user)
-   const users = await User.find();
+   // console.log(req.user)
+   const users = await User.find().select('-password');
    res.status(200).json(users)
+ })
+
+ /**-----------------------------------------------
+ * @desc    Get User Profile
+ * @route   /api/users/profile/:id
+ * @method  GET 
+ * @access  public
+ ------------------------------------------------*/
+ const getUserProfileCtr = asyncHandler(async(req, res) => {
+   const user = await User.findById(req.params.id).select('-password')
+   // console.log(user)
+   if (!user) {
+      return res.status(404).json({ message: "User Not Found" })
+   }
+   res.status(200).json(user)
  })
 
  module.exports = {
     getUsersController,
+    getUserProfileCtr,
  }
--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,8 +1,11 @@
-const { getUsersController } = require('../controllers/usersController');
+const { getUsersController, getUserProfileCtr } = require('../controllers/usersController');
 const { verifyTokenAndAdmin } = require('../middlewares/verifyToken');
 const router = require('express').Router()
 
 // /api/users/profile
 router.get("/profile", verifyTokenAndAdmin, getUsersController);
+
+// /api/users/profile/:id
+router.get("/profile/:id", getUserProfileCtr);
 
 module.exports = router;
