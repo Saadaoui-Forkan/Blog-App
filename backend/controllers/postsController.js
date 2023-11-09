7ba062ddfacd1cc0f80681d11f826fca41aeb8c7
unknown <forkan.saadaoui@gmail.com>
2023-11-09 22:02:01 +0100

    get posts

--- backend/controllers/postsController.js
+++ backend/controllers/postsController.js
@@ -1,10 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const asyncHandler = require("express-async-handler");
-const {
-  Post,
-  validateCreatePost,
-} = require("../models/Post");
+const { Post, validateCreatePost } = require("../models/Post");
 const {
   cloudinaryUploadImage,
   cloudinaryRemoveImage,
@@ -16,41 +13,87 @@ const {
  * @method  POST
  * @access  private (only logged in user)
  ------------------------------------------------*/
-const createPostCtr = asyncHandler(async(req, res)=>{
-    // Image Validation
-    if (!req.file) {
-        return res.status(400).json({ message: "no image provided" })
-    }
+const createPostCtr = asyncHandler(async (req, res) => {
+  // Image Validation
+  if (!req.file) {
+    return res.status(400).json({ message: "no image provided" });
+  }
 
-    // Data Validation
-    const { error } = validateCreatePost(req.body);
-    if (error) {
-        return res.status(400).json({ message: error.details[0].message });
-    }
+  // Data Validation
+  const { error } = validateCreatePost(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
 
-    // Upload Photo
-    const imagePath = path.join(__dirname, `../images/${req.file.filename}`);
-    const result = await cloudinaryUploadImage(imagePath);
+  // Upload Photo
+  const imagePath = path.join(__dirname, `../images/${req.file.filename}`);
+  const result = await cloudinaryUploadImage(imagePath);
 
-    // Save new post in database
-    const post = await Post.create({
-        title: req.body.title,
-        description: req.body.description,
-        category: req.body.category,
-        user: req.user.id,
-        image: {
-          url: result.secure_url,
-          publicId: result.public_id,
-        },
-    })
+  // Save new post in database
+  const post = await Post.create({
+    title: req.body.title,
+    description: req.body.description,
+    category: req.body.category,
+    user: req.user.id,
+    image: {
+      url: result.secure_url,
+      publicId: result.public_id,
+    },
+  });
 
-    // Send response to the client
-    res.status(201).json(post);
+  // Send response to the client
+  res.status(201).json(post);
 
-    // 6. Remove image from the server
-    fs.unlinkSync(imagePath);
-})
+  // 6. Remove image from the server
+  fs.unlinkSync(imagePath);
+});
+
+/**-----------------------------------------------
+ * @desc    Get All Posts
+ * @route   /api/posts
+ * @method  GET
+ * @access  public
+ ------------------------------------------------*/
+const getAllPostsCtr = asyncHandler(async (req, res) => {
+  const POST_PER_PAGE = 3;
+  const { pageNumber, category } = req.query;
+  let posts;
+
+  if (pageNumber) {
+    posts = await Post.find()
+      .skip((pageNumber - 1) * POST_PER_PAGE)
+      .limit(POST_PER_PAGE)
+      .sort({ createdAt: -1 })
+      .populate("user", ["-password", "-email"]);
+  } else if (category) {
+    posts = await Post.find({ category })
+      .sort({ createdAt: -1 })
+      .populate("user", ["-password", "-email"]);
+  } else {
+    posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .populate("user", ["-password", "-email"]);
+  }
+  res.status(200).json(posts);
+});
+
+/**-----------------------------------------------
+ * @desc    Get Single Post
+ * @route   /api/posts/:id
+ * @method  GET
+ * @access  public
+ ------------------------------------------------*/
+ const getSinglePostCtr = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id).populate("user", ["-password"]);
+  if (!post) {
+    return res.status(404).json({ msg: "Post Not Found" })
+  }
+  res.status(200).json(post);
+});
+
 
 module.exports = {
-    createPostCtr,
-}
+  createPostCtr,
+  getAllPostsCtr,
+  getSinglePostCtr,
+};
--- backend/routes/postsRoute.js
+++ backend/routes/postsRoute.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
-const { createPostCtr } = require("../controllers/postsController");
+const { createPostCtr, getAllPostsCtr, getSinglePostCtr } = require("../controllers/postsController");
 const photoUpload = require("../middlewares/photoUpload");
+const validateObjectId = require("../middlewares/validateObjectId");
 const { verifyToken } = require("../middlewares/verifyToken");
 
 // /api/posts
 router.post("/", verifyToken, photoUpload.single('image'), createPostCtr);
+
+//  /api/posts
+router.get("/", validateObjectId, getAllPostsCtr);
+
+//  /api/posts/:id
+router.get('/:id', validateObjectId, getSinglePostCtr)
 
 module.exports = router
