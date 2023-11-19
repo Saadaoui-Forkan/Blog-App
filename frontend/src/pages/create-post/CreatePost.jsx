923ec41706bdc6f87482b631d076b56e3a54fe08
unknown <forkan.saadaoui@gmail.com>
2023-11-19 21:52:10 +0100

    create post form

--- frontend/src/pages/create-post/CreatePost.jsx
+++ frontend/src/pages/create-post/CreatePost.jsx
@@ -1,8 +1,86 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './create-post.css'
+import { ToastContainer, toast } from 'react-toastify'
 
 function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [file, setFile] = useState(null);
+
+  // Form Submit Handler
+  const formSubmitHandler = (e) => {
+    e.preventDefault()
+    if (title.trim() === "") return toast.error("Post Title is required");
+    if (category.trim() === "") return toast.error("Post Category is required");
+    if (description.trim() === "")
+      return toast.error("Post Description is required");
+    if (!file) return toast.error("Post Image is required");
+    
+    const formData = new FormData();
+    formData.append("image", file);
+    formData.append("title", title);
+    formData.append("description", description);
+    formData.append("category", category);
+  }
+
   return (
-    <div>CreatePost</div>
+    <section className="create-post">
+      <ToastContainer theme='colored' />
+      <h1 className="create-post-title">Create New Post</h1>
+      <form className="create-post-form" onSubmit={formSubmitHandler}>
+        <input
+          type="text"
+          placeholder="Post Title"
+          className="create-post-input"
+          value={title}
+          onChange={(e)=>setTitle(e.target.value)}
+        />
+        <select
+          className="create-post-input"
+          value={category}
+          onChange={(e)=>setCategory(e.target.value)}
+        >
+          <option disabled value="">
+            Select A Category
+          </option>
+            <option value="music">
+              music
+            </option>
+            <option value="coffee">
+              coffee
+            </option>
+        </select>
+        <textarea
+          className="create-post-textarea"
+          rows="5"
+          placeholder="Post Description"
+          value={description}
+          onChange={(e)=>setDescription(e.target.value)}
+        ></textarea>
+        <input
+          type="file"
+          name="file"
+          id="file"
+          className="create-post-upload"
+          onChange={(e)=>setFile(e.target.files[0])}
+        />
+        <button type="submit" className="create-post-btn">
+          {/* {loading ? (
+            <RotatingLines
+              strokeColor="white"
+              strokeWidth="5"
+              animationDuration="0.75"
+              width="40"
+              visible={true}
+            />
+          ) : (
+            "Create"
+          )} */}
+          create
+        </button>
+      </form>
+    </section>
   )
 }
 
